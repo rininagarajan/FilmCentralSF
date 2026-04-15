d7d9e3f9c3414868ca3a06b51fccb88c790a4ddf
rininagarajan <rininagarajan@gmail.com>
2026-04-15 13:23:41 -0400

    Add files via upload

---
+++ jquery/java/js.js
@@ -0,0 +1,24 @@
+// alert ("Pages linked!")
+
+/*var firstVar = 1; 
+var secondVar = "Text content here"
+var thirdVar = [0, 24, 69, 420]
+var x = 8
+
+console.log ("hi")
+console.log (firstVar)
+console.log (secondVar)
+console.log (thirdVar)
+
+/*if(this is true) {
+    then do this
+}
+*/
+
+/*if (x == 0) {
+    console.log ("X IS 0")
+}
+else {
+    console.log ("x IS NOT 0")
+}*/
+
